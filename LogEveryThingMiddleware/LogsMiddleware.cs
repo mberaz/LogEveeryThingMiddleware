d1b1a6c3206c32614c51d44930b1591f4af26186
michael berezin <mbearz@gmail.com>
2024-03-09 23:58:09 +0200

    first commit

---
+++ LogEveryThingMiddleware/LogsMiddleware.cs
@@ -0,0 +1,23 @@
+﻿namespace LogEveryThingMiddleware
+{
+    public class LogsMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LogsMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            httpContext.Request.Headers.TryGetValue("x-should-log", out var shouldLog);
+
+            //https://stackoverflow.com/questions/50747749/how-to-use-httpclienthandler-with-httpclientfactory-in-net-core
+
+            // Call the next delegate/middleware in the pipeline.
+            await _next(httpContext);
+
+        }
+    }
+}
